98aa03d34329f8c6a87fef2a447bbac8a0b02333
suleiman-duwane <suleimanduwane90@gmail.com>
2024-03-04 15:33:37 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, file_content = 'This is a test content.'):
+    with open(f"{file_name}.txt", mode="w" ) as any_file:
+        any_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a" ) as any_file:
+        any_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r" ) as any_file:
+        return any_file.read()
